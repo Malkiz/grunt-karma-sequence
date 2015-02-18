ed16e67bd557edc55ace634cf122d29a621d43ae
omri <omri.ey@walkme.com>
2015-02-18 14:49:11 +0200

    first commit

---
+++ tasks/sequence.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.registerMultiTask('karma-sequence', 'run karma with multiple browsers sequentially', function(){
+		var _this = this;
+		var browsers = _this.data.browsers;
+
+		if (typeof browsers == 'object' && browsers.length > 0) {
+			browsers.forEach(function (browser) {
+				var clonedData = clone(_this.data);
+				clonedData.browsers = [browser];
+
+				var taskName = 'karma';
+				var name = _this.target + '-sequence-' + browser;
+				grunt.config(taskName + '.' + name, clonedData);
+				grunt.task.run(taskName + ':' + name);
+			});
+		}
+	});
+
+	function clone(obj) {
+		return JSON.parse(JSON.stringify(obj));
+	}
+}
